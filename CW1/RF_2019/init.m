ba0dcf1d3d205f0c2d2448cbc761120bc32654a5
Peter Miller <peter110@hotmail.co.uk>
2019-01-27 17:29:21 +0000

    updated SIFT library

--- CW1/RF_2019/init.m
+++ CW1/RF_2019/init.m
@@ -9,7 +9,7 @@ addpath('./external');
 addpath('./external/libsvm-3.18/matlab');
 
 % initialise external libraries
-run('external/vlfeat-0.9.18/toolbox/vl_setup.m'); % vlfeat library
+run('external/vlfeat-0.9.21/toolbox/vl_setup.m'); % vlfeat library
 cd('external/libsvm-3.18/matlab'); % libsvm library
 run('make');
 cd('../../..');
