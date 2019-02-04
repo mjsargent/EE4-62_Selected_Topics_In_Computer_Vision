032cae1c5b9c4e254c5a7c5858e5cd110d9ef961
Peter Miller <peter110@hotmail.co.uk>
2019-02-04 21:21:23 +0000

    added sweep loops for cw1_main (q2)

--- CW1/RF_2019/cw1_main.m
+++ CW1/RF_2019/cw1_main.m
@@ -5,23 +5,46 @@ init; clc;
 vocab_size = 200;
 [data_train, data_test] = getData(vocab_size);
 %% 2. Random forest training
+
 % choose tree options
 % number_runs = 5;
-forest_options = struct; 
-forest_options.depth = 5; % [2,5,8,12]
-forest_options.numTrees = 10000; % [100,1000,10000]
-forest_options.numSplits = 30; % [20,50,100]
-forest_options.verbose = true; % outputs training update
-forest_options.classifierId = 1; % [1,2,5]
-forest_options.classifierCommitFirst = false;
-forest_options.bagSizes = 150; % [50,100,200]
-forest_options.decChoice = 1; % [1,2] / 1 = entropy, 2 = Gini index
+depth = [2,5,8,12];
+numTree = [100,1000,10000];
+numSplits = [20,50,100];
+classifierId = [1,2,5];
+classifierCommitFirst = false;
+bagSizes = [50,100,200];
+decChoice = [2]; % / 1 = entropy, 2 = Gini index
 % standardise data to zero mean, unit variance  === DON'T DO THIS ===
 %data_train = bsxfun(@rdivide, bsxfun(@minus, data_train, mean(data_train)), var(data_train) + 1e-10);
 %data_test = bsxfun(@rdivide, bsxfun(@minus, data_test, mean(data_train)), var(data_train) + 1e-10);
 true_class_vector = reshape((ones(10,15).*[1:10]')',[150,1]);
-% train the forest
-forest_model = forestTrain(data_train,true_class_vector,forest_options);
-predicted_class_vector = forestTest(forest_model,data_test);
-% results
-results = sum(~logical(true_class_vector-predicted_class_vector))/length(true_class_vector)
+%%
+results_store = zeros(length(depth),length(numTree),length(numSplits),length(classifierId),length(bagSizes),length(decChoice));
+forest_options = struct;
+forest_options.verbose = false; % outputs training update
+forest_options.classifierCommitFirst = false;
+for depth_idx = 1:length(depth)
+    forest_options.depth = depth(depth_idx);
+    for numTree_idx = 1:length(numTree)
+        forest_options.numTrees = numTree(numTree_idx);
+        for numSplits_idx = 1:length(numSplits)
+            forest_options.numSplits = numSplits(numSplits_idx);
+            for classifierId_idx = 1:length(classifierId)
+                forest_options.classifierId = classifierId(classifierId_idx);
+                for bagSizes_idx = 1:length(bagSizes)
+                    forest_options.bagSizes = bagSizes(bagSizes_idx);
+                    for decChoice_idx = 1:length(decChoice)
+                        forest_options.decChoice = decChoice_idx(decChoice_idx);
+                        % train the forest
+                        forest_model = forestTrain(data_train,true_class_vector,forest_options);
+                        predicted_class_vector = forestTest(forest_model,data_test);
+                        % results
+                        results_store(depth_idx,numTree_idx,numSplits_idx,classifierId_idx,bagSizes_idx,decChoice_idx) = sum(~logical(true_class_vector-predicted_class_vector))/length(true_class_vector);
+                    end
+                end
+            end
+        end
+    end
+end
+save('Q2_sweep_coarse.mat',results_store);
